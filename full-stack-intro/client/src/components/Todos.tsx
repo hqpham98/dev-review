29285abf388015c4e5a4961356aa0c72e850fecb
Henry Pham <hqpham98@gmail.com>
2025-03-09 18:36:44 -0700

    Cleaned up code in Todos.tsx

--- full-stack-intro/client/src/components/Todos.tsx
+++ full-stack-intro/client/src/components/Todos.tsx
@@ -17,14 +17,14 @@ export function Todos() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<unknown>();
-  const [toggle, setToggle] = useState(false);
 
   /* Implement useEffect to fetch all todos. Hints are at the bottom of the file. */
   useEffect(() => {
     async function getTodos() {
       try {
         const response = await fetch("/api/todos");
-        const todosList = await response.json();
+        if (!response.ok) throw new Error(`fetch Error ${response.status}`);
+        const todosList = (await response.json()) as Todo[];
         setTodos(todosList);
       } catch (err) {
         setError(err);
@@ -33,19 +33,18 @@ export function Todos() {
       }
     }
     getTodos();
-  }, [toggle]);
+  }, []);
 
   /* Implement addTodo to add a new todo. Hints are at the bottom of the file. */
   async function addTodo(newTodo: UnsavedTodo) {
     try {
       const response = await fetch("/api/todos", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newTodo),
       });
-      const todo = await response.json();
+      if (!response.ok) throw new Error(`fetch Error ${response.status}`);
+      const todo = (await response.json()) as Todo;
       setTodos((prevTodos) => [...prevTodos, todo]);
     } catch (err) {
       setError(err);
@@ -54,19 +53,19 @@ export function Todos() {
 
   /* Implement toggleCompleted to toggle the completed state of a todo. Hints are at the bottom of the file. */
   async function toggleCompleted(todo: Todo) {
-    const { task, isCompleted } = todo;
+    todo.isCompleted = !todo.isCompleted;
     try {
       const response = await fetch(`/api/todos/${todo.todoId}`, {
         method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ task, isCompleted: !isCompleted }),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ todo }),
       });
-
-      if (response.ok) {
-        setToggle((prev) => !prev);
-      }
+      if (!response.ok) throw new Error(`fetch Error ${response.status}`);
+      const updated = (await response.json()) as Todo;
+      const newTodos = todos.map((original) =>
+        original.todoId === updated.todoId ? updated : original
+      );
+      setTodos(newTodos);
     } catch (err) {
       setError(err);
     }
@@ -95,57 +94,3 @@ export function Todos() {
     </div>
   );
 }
-
-// ----------- HINTS!!!!!!!!! ------------
-/* useEffect:
- *  Use fetch to send a GET request to `/api/todos`.
- *  Once the response JSON is received and parsed,
- *  update state with the received todos.
- *  Remember to use type assertion when retrieving the JSON from the response.
- *  Handle errors correctly and set `error` to any error or error status code.
- *  Set `isLoading` to false when all network activity is finished.
- */
-
-/* addTodo:
- *  Use fetch to send a POST request to `/api/todos`.
- *  Once the response JSON is received and parsed,
- *    - set the Todos to a _new_ array with the added Todo concatenated
- *    - handle errors appropriately (as mentioned above)
- *  Remember to use type assertion when retrieving the JSON from the response.
- *
- *  DO NOT MUTATE the original state array, nor any objects within it.
- *  Reminder: A React component will re-render if one of its state setters
- *  is called, but only if the new value is different than the old value.
- *  With arrays and objects, the new value will be different _only if_ a
- *  new array or object instance is created. That is why mutating the original
- *  state array _will not_ trigger a re-render.
- *  See https://react.dev/learn/updating-arrays-in-state for more info.
- *
- *  TIP: When calling fetch, be sure to SERIALIZE the todo object in the body
- *       with JSON.stringify() and specify the "Content-Type" header as "application/json"
- *  TIP: Use Array.prototype.concat to create a new array containing the contents
- *       of the old array, plus the object returned by the server.
- *       Alternatively, use the spread operator: `[...oldArray, addedItem]`
- */
-
-/* toggleCompleted:
- *  Toggle the todo's "isCompleted" status.
- *  Use fetch to send a PUT request to `/api/todos/${todoId}` with the modified `todo`
- *  in the body.
- *  Once the response JSON is received and parsed,
- *    - create a shallow copy of the todos array from state
- *    - replace the old todo with the todo received from the server
- *    - handle errors appropriately (as mentioned above)
- *  Remember to use type assertion when retrieving the JSON from the response.
- *
- *  NOTE: "toggle" means to flip back and forth, so clicking a todo
- *  in the list repeatedly should "toggle" its isCompleted status back and forth.
- *
- *  DO NOT MUTATE the original state array, nor any objects within it. See above.
- *
- *  TIP: When calling fetch, be sure to SERIALIZE the todo object in the body
- *       with JSON.stringify() and specify the "Content-Type" header as "application/json"
- *  TIP: `Array.map` is a convenient way to make a copy of an array -- compare
- *       the Todo IDs and return the existing Todo if it is not the one that was
- *       just updated, and return the updated array if it is.
- */
